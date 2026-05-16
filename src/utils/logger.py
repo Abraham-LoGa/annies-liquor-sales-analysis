7a074c6961d4995d27a2c2800bf0afa2dbc4b2db
Abraham-LoGa <a.loga.wspace@gmail.com>
2026-05-15 23:34:40 -0600

    feat: add logger utility

---
+++ src/utils/logger.py
@@ -0,0 +1,9 @@
+from config import LOG_LEVEL
+import logging
+
+logging.basicConfig(
+    level=LOG_LEVEL,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger(__name__)
